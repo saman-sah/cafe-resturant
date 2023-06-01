c95a156aec06aa255487f711d60533c27e396710
SamanSah <sahraei.saman91@gmail.com>
2023-06-01 10:47:57 +0330

    implement primitive design

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,26 @@
 <template>
-  <q-page class="flex">
+  <q-page class="flex home">
     <div class="row">
-      <div class="col-6 item q-pa-md">
-        <q-card class="my-card" flat bordered>
+      
+      <div v-for="n in 6"
+      :key="n"
+      class="col-6 item q-pa-md">
+        <q-card class="my-card bg-dark card-item-store" flat bordered >
           <q-img src="https://source.unsplash.com/random/550x366/?fastfood" />
 
           <q-card-section class="q-py-sm">
             <q-btn
-              fab
-              color="primary"
+              round
+              color="secondary"
               icon="place"
               class="absolute"
+              size="md"
               style="top: 0; right: 12px; transform: translateY(-50%);"
-              @click="details= !details"
+              @click="location= !location"
             />
 
             <div class="row no-wrap items-center">
-              <div class="col text-h6 ellipsis">
+              <div class="col text-h6 ellipsis text-primary">
                 <q-item
                 clickable
                 tag="a"
@@ -28,14 +32,14 @@
           </q-card-section>
 
           <q-card-section class="q-pt-none">
-            <div class="text-caption text-grey">
+            <div class="text-caption text-white">
               Small plates, salads & sandwiches in an intimate setting.
             </div>
           </q-card-section>
 
-          <q-separator />
+          <!-- <q-separator /> -->
 
-          <q-card-actions class="row justify-between">
+          <!-- <q-card-actions class="row justify-between">
             <q-btn-dropdown color="primary" label="" flat icon="menu" menu-self="bottom end">
               <q-list>
 
@@ -48,58 +52,7 @@
               </q-list>
             </q-btn-dropdown>
             <span>rate: 4.5</span>
-          </q-card-actions>
-        </q-card>
-      </div>
-      <div class="col-6 item q-pa-md">
-        <q-card class="my-card" flat bordered>
-          <q-img src="https://source.unsplash.com/random/550x366/?cafe" />
-
-          <q-card-section class="q-py-sm">
-            <q-btn
-              fab
-              color="primary"
-              icon="place"
-              class="absolute"
-              style="top: 0; right: 12px; transform: translateY(-50%);"
-              @click="details= !details"
-            />
-
-            <div class="row no-wrap items-center">
-              <div class="col text-h6 ellipsis">
-                <q-item
-                clickable
-                tag="a"
-                to="/store/cafe"
-                >
-                  Cafe
-                </q-item>
-              </div>
-            </div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            <div class="text-caption text-grey">
-              Small plates, salads & sandwiches in an intimate setting.
-            </div>
-          </q-card-section>
-
-          <q-separator />
-
-          <q-card-actions class="row justify-between">
-            <q-btn-dropdown color="primary" label="" flat icon="menu" menu-self="bottom end">
-              <q-list>
-
-                <q-item clickable v-close-popup @click="onItemClick">
-                  <q-item-section>
-                    <q-item-label>Videos</q-item-label>
-                  </q-item-section>
-                </q-item>
-
-              </q-list>
-            </q-btn-dropdown>
-            <span>rate: 4.5</span>
-          </q-card-actions>
+          </q-card-actions> -->
         </q-card>
       </div>
       
@@ -107,13 +60,13 @@
     
   </q-page>
 
-  <q-dialog v-model="details">
-    <detailsStore />    
+  <q-dialog v-model="location">
+    <locationStore />    
   </q-dialog>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import detailsStore from '../components/modals/Details.vue'
-const details= ref(false)
+import locationStore from '../components/modals/Location.vue'
+const location= ref(false)
 </script>
---
+++ src/pages/Recipes.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="q-pa-md" >
+      <q-list dense bordered padding class="rounded-borders">
+        <q-item clickable v-ripple>
+          <q-item-section>Icon as avatar</q-item-section>
+          <q-item-section avatar>
+            80gr
+          </q-item-section>
+        </q-item>
+  
+        <q-item clickable v-ripple>
+          <q-item-section>Avatar-type icon</q-item-section>
+          <q-item-section avatar>
+            80gr
+          </q-item-section>
+        </q-item>
+  
+        <q-item clickable v-ripple>
+          <q-item-section>Rounded avatar-type icon</q-item-section>
+          <q-item-section avatar>
+            80gr
+          </q-item-section>
+        </q-item>
+  
+        <q-item clickable v-ripple>
+          <q-item-section>Letter avatar-type</q-item-section>
+          <q-item-section avatar>
+               80gr
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </div>
+  </template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
